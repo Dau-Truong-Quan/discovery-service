8fbc9771376d515908074e18541903ea47c4a0f8
DESKTOP-CESMAPL\PC <Quan Dau>
2026-04-20 23:45:19 +0700

    first commit

---
+++ src/test/java/garden/quan/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package garden.quan.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
